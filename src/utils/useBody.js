00e08b4111c20b2c1e9bf587890f546ac3cead6b
Yashwith Salian <92640662+yashwith26@users.noreply.github.com>
2024-11-10 12:50:30 +0530

    Revert "added cors proxy plugin"

--- src/utils/useBody.js
+++ src/utils/useBody.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { MENU_API } from "./constants";
+
 const useBody = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
   const [filteredRestaurants, setFilteredRestaurants] = useState([]);
@@ -10,7 +10,7 @@ const useBody = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0009342&lng=77.7631425&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0009342&lng=77.7631425&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
 
